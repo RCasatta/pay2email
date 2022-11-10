bbc587f657bcc11eb751d86f955587f140913530
Riccardo Casatta <riccardo@casatta.it>
2022-11-10 17:14:37 +0100

    handle last-modified

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,10 @@ mod error;
 mod qr;
 mod routes;
 
+use chrono::{DateTime, Utc};
 pub use error::Error;
 use rocket::fs::NamedFile;
+use rocket::http::hyper::header::{CACHE_CONTROL, IF_MODIFIED_SINCE, LAST_MODIFIED};
 use rocket::http::Status;
 use rocket::response::Responder;
 use rocket::{response, Request, Response};
@@ -25,26 +27,41 @@ use std::path::{Path, PathBuf};
 #[database("diesel")]
 pub struct Db(diesel::SqliteConnection);
 
-struct CachedFile(NamedFile);
+struct CachedFile(NamedFile, String);
 
 impl<'r> Responder<'r, 'static> for CachedFile {
     fn respond_to(self, req: &'r Request<'_>) -> response::Result<'static> {
-        Response::build_from(self.0.respond_to(req)?)
-            .raw_header("Cache-control", "max-age=86400") //  24h (24*60*60)
-            .ok()
+        if req
+            .headers()
+            .get(IF_MODIFIED_SINCE.as_str())
+            .any(|s| s == &self.1)
+        {
+            Response::build().status(Status::NotModified).ok()
+        } else {
+            Response::build_from(self.0.respond_to(req)?)
+                .raw_header(CACHE_CONTROL.as_str(), "max-age=86400") //  24h (24*60*60)
+                .raw_header(LAST_MODIFIED.as_str(), self.1)
+                .ok()
+        }
     }
 }
 
 #[get("/<file..>")]
 async fn files(file: PathBuf) -> Option<CachedFile> {
     let buf = Path::new("static/").join(file);
-    if buf.is_dir() {
-        NamedFile::open(buf.join("index.html"))
-            .await
-            .ok()
-            .map(|nf| CachedFile(nf))
+    let file = if buf.is_dir() {
+        buf.join("index.html")
     } else {
-        NamedFile::open(buf).await.ok().map(|nf| CachedFile(nf))
+        buf
+    };
+    match NamedFile::open(&file).await {
+        Ok(named_file) => {
+            let metadata = std::fs::metadata(&file).ok()?;
+            let last_modified: DateTime<Utc> = metadata.modified().ok()?.into();
+            let last_modified = last_modified.to_rfc3339();
+            Some(CachedFile(named_file, last_modified))
+        }
+        Err(_) => None,
     }
 }
 
