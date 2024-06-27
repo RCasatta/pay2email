27d41e7b48e7516b41e6cd63a59b75de59f6e3f7
Riccardo Casatta <riccardo@casatta.it>
2024-06-27 18:26:15 +0200

    updates

--- node-side/on_pay.py
+++ node-side/on_pay.py
@@ -12,13 +12,14 @@ def init(options, configuration, plugin):
 
 
 @plugin.subscribe("invoice_payment")
-def on_invoice_payment(plugin, invoice_payment):
-    plugin.log("on_pay on_invoice_payment")
+def on_payment(plugin, invoice_payment, **kwargs):
+    plugin.log("Received invoice_payment event for label {label}, preimage {preimage},"
+               " and amount of {msat}".format(**invoice_payment))
     url = plugin.options['on-pay-notify-url']['value']
     s = requests.Session()
-    s.auth = (os.getenv("HTTP_AUTH_USER"), os.getenv("HTTP_AUTH_PASSWORD"))
-    s.post(url, data=invoice_payment.preimage)
-    # TODO, if not 200 save somewhere and retry later
+    s.auth = (os.getenv("HTTP_USER"), os.getenv("HTTP_PSW"))
+    s.post(url, data=invoice_payment['preimage'])
+    # TODO, if faling save somewhere and retry later
 
 
 plugin.add_option('on-pay-notify-url', 'https://pay2.email/invoice/paid', 'The url where to notify the paid invoice')
--- node-side/upload_invoices.py
+++ node-side/upload_invoices.py
@@ -6,12 +6,12 @@ import random
 import os
 
 s = requests.Session()
-s.auth = (os.getenv("HTTP_AUTH_USER"), os.getenv("HTTP_AUTH_PASSWORD"))
+s.auth = (os.getenv("HTTP_USER"), os.getenv("HTTP_PSW"))
 r = s.get('https://pay2.email/invoice/count')
 available = int(r.text)
 print(available)
 l1 = LightningRpc(os.getenv("LIGHTNING_RPC"))
 for i in range(available, 60):
-    expire = "{}d".format(random.randint(7, 28))  # random so that invoices don't expire at the same time
+    expire = "{}".format(random.randint(504800, 704800))  # about a week random so that invoices don't expire at the same time
     invoice = l1.invoice("20sat", str(uuid.uuid4()), "pay2.email", expire)['bolt11']
     s.post('https://pay2.email/invoice', invoice)
